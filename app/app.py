8b6d5a916eb481f73d5d75a63805706108023579
Manav M <manav.maj@outlook.com>
2023-04-20 12:23:33 -0400

    Created login and added backend

--- app/app.py
+++ app/app.py
@@ -1,6 +1,70 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, flash, render_template, request, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
+from models import db, User, Dorm, Rating, Vote
+from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'your-secret-key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dorm_rating.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'signin'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            flash('Logged in successfully.', 'success')
+            return redirect(url_for('ratings'))
+        else:
+            flash('Invalid email or password.', 'danger')
+    return render_template('signin.html')
+
+@app.route('/dorms')
+def dorms():
+    all_dorms = Dorm.query.all()
+    return render_template('dorms.html', dorms=all_dorms)
+
+@app.route('/dorms/<int:dorm_id>')
+def dorm_page(dorm_id):
+    dorm = Dorm.query.get_or_404(dorm_id)
+    ratings = Rating.query.filter_by(dorm_id=dorm_id).all()
+    return render_template('dorm_page.html', dorm=dorm, ratings=ratings)
+
+@app.route('/add_dorm', methods=['GET', 'POST'])
+def add_dorm():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        image_url = request.form['image_url']
+        maps_url = request.form['maps_url']
+
+        new_dorm = Dorm(name=name, description=description, image_url=image_url, maps_url=maps_url)
+        db.session.add(new_dorm)
+        db.session.commit()
+
+        flash('Dorm added successfully', 'success')
+        return redirect(url_for('dorms'))
+
+    return render_template('add_dorm.html')
+
 
 @app.route('/')
 def dashboard():
@@ -28,10 +92,14 @@ def dashboard():
 def about():
     return render_template('about.html')
 
-@app.route('/signin')
-def signin():
-    return render_template('signin.html')
+@app.route('/ratings')
+@login_required
+def ratings():
+    user_ratings = Rating.query.filter_by(user_id=current_user.id).all()
+    return render_template('ratings.html', ratings=user_ratings)
+
 
 
 if __name__ == '__main__':
+    create_tables() 
     app.run(debug=True)
--- app/models.py
+++ app/models.py
@@ -1,48 +1,31 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
 class Dorm(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    description = db.Column(db.Text)
-    image_url = db.Column(db.String)
-    maps_url = db.Column(db.String)
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    image_url = db.Column(db.String(256), nullable=True)
+    maps_url = db.Column(db.String(256), nullable=True)
 
-    ratings = db.relationship('Rating', backref='dorm', lazy=True)
-
-    def __repr__(self):
-        return f'<Dorm {self.name}>'
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(64), nullable=False, unique=True)
+    email = db.Column(db.String(128), nullable=False, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
-
-    ratings = db.relationship('Rating', backref='user', lazy=True)
-    votes = db.relationship('Vote', backref='user', lazy=True)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
 
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     dorm_id = db.Column(db.Integer, db.ForeignKey('dorm.id'), nullable=False)
     rating = db.Column(db.Float, nullable=False)
-    comment = db.Column(db.Text)
-
-    votes = db.relationship('Vote', backref='rating', lazy=True)
-
-    def __repr__(self):
-        return f'<Rating {self.id} by User {self.user_id} for Dorm {self.dorm_id}>'
+    comment = db.Column(db.Text, nullable=True)
+    user = db.relationship('User', backref=db.backref('ratings', lazy=True))
 
 class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rating_id = db.Column(db.Integer, db.ForeignKey('rating.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     vote_type = db.Column(db.Enum('upvote', 'downvote', name='vote_types'), nullable=False)
-
-    def __repr__(self):
-        return f'<Vote {self.vote_type} by User {self.user_id} for Rating {self.rating_id}>'
